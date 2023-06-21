7298fd74120a7d7c06e7e396b21d8ce1a20bfa5c
Aju <ajees2k2@gmail.com>
2023-06-21 06:19:33 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "../stytles/modern-normalize.css";
+import "../stytles/main.css";
+import "../stytles/components/header.css";
+import "../stytles/components/hero.css";
+import "../stytles/components/about.css";
+import "../stytles/components/footer.css";
+import "../stytles/components/contact.css";
+import "../stytles/components/mobile-nav.css";
+import "../stytles/components/animated-text.css";
+import "../stytles/utils.css";
+
+import mobileNav from "./utils/mobile-nav";
+import lightMode from "./utils/light-mode";
+
+mobileNav();
+lightMode();
